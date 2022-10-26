0976f1c0fa07e5e6ad42e74b6f43b60f7ac9d2bd
vanduoc <vanduoc198@gmail.com>
2022-10-26 23:13:09 +0700

    install libraries, nodemon...boostrap4

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const { engine } = require('express-handlebars');
+const morgan = require('morgan');
+const app = express();
+const port = 3001;
+
+app.use(express.static(path.join(__dirname,'public')))
+
+// HTTP request logger middleware for node.js
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'))
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
