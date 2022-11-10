0318ed44f1c80c4ab13098616a40c0a6756c850d
vanduoc <vanduoc198@gmail.com>
2022-11-10 16:01:57 +0700

    sort middleware

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,7 +4,15 @@ const Course = require('../models/Courses');
 class MeController {
   //[GET]/me/store/courses
   storedCourses(req, res, next) {
-    Promise.all([Course.find({}), Course.countDeleted()])
+    let coursesQuery = Course.find({});
+
+    if (req.query.hasOwnProperty('_sort')) {
+      coursesQuery = coursesQuery.sort({
+        [req.query.column]: req.query.type,
+      });
+    }
+
+    Promise.all([coursesQuery, Course.countDeleted()])
       .then(([courses, numCoursesDeleted]) => {
         res.render('me/store-courses', {
           courses: multipleMongooseToObject(courses),
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const app = express();
 const port = 3001;
 
 const db = require('./config/db');
+const sortMiddleware = require('./app/middlewares/SortMiddleware');
 
 //connect DB
 db.connect();
@@ -28,6 +29,9 @@ app.use(express.json());
 //Lets we use HTTP verbs such as PUT or DELETE in places where the client doesn’t support it.
 app.use(methodOverride('_method'));
 
+// use custom sort middleware
+app.use(sortMiddleware);
+
 // Template engine
 app.engine(
   'hbs',
@@ -35,6 +39,28 @@ app.engine(
     extname: '.hbs',
     helpers: {
       sum: (a, b) => a + b,
+      sortable: (field, sort) => {
+        let sortType = field === sort.column ? sort.type : 'default';
+
+        const icons = {
+          default: 'funnel-outline',
+          asc: 'chevron-up-circle-outline',
+          desc: 'chevron-down-circle-outline',
+        };
+
+        const types = {
+          default: 'asc',
+          asc: 'desc',
+          desc: 'asc',
+        };
+
+        let icon = icons[sortType];
+        const type = types[sortType];
+
+        return `<a href="?_sort&column=${field}&type=${type}">
+                  <ion-icon name="${icon}"></ion-icon>
+                </a>`;
+      },
     },
   }),
 );
